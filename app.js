4debd19226619738581f1b0d90557cd706e3714c
Taraka Saikumar <67173900+TarakaSaiKumar@users.noreply.github.com>
2025-07-07 18:24:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -108,7 +108,8 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 })
 
-app.listen(8080, () => {
-    console.log("Server is listening port 8080")
-})
+const PORT = process.env.PORT || 8080; 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
 
